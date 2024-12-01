f58118b587081fb748159bc8ada43c8d52214843
GitAdamReed <adamreed170@gmail.com>
2024-12-01 16:07:37 +0000

    Day 1 Part 1 Complete

---
+++ AdventOfCode2024/AdventOfCode2024/Classes/Day1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Classes
+{
+    public class Day1 : Day
+    {
+        private static readonly string _inputPath = GetInputPath("Day1.txt");
+        private static readonly string _outputPath = GetOutputPath("Day1.txt");
+        
+        public Day1()
+        {
+            CreateOutput();
+        }
+
+        private static int CalculateTotalDistance()
+        {
+            int sum = 0;
+            List<int> left = [];
+            List<int> right = [];
+            
+            foreach (var line in File.ReadLines(_inputPath))
+            {
+                var splitLine = line.Split(' ');
+                left.Add(Int32.Parse(splitLine.First()));
+                right.Add(Int32.Parse(splitLine.Last()));
+            }
+
+            left.Sort();
+            right.Sort();
+            
+            for (int i = 0; i < left.Count; i++) sum += Int32.Abs(left[i] - right[i]);
+
+            return sum;
+        }
+        
+        protected override void CreateOutput()
+        {
+            File.WriteAllText(_outputPath, $"Part 1: {CalculateTotalDistance()}");
+        }
+    }
+}
--- AdventOfCode2024/AdventOfCode2024/Program.cs
+++ AdventOfCode2024/AdventOfCode2024/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace AdventOfCode2024
+﻿using AdventOfCode2024.Classes;
+
+namespace AdventOfCode2024
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Day1
+            //new Day1();
+            #endregion
         }
     }
 }
